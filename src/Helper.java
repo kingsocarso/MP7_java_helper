431c1c08219a841fc3724ff8755257f67de5153d
kingsocarso <kingsocarso@gmail.com>
2017-12-11 10:42:59 -0600

    Initialized scanner

--- src/Helper.java
+++ src/Helper.java
@@ -1,10 +1,20 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Helper {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		File file = new File("NRHP_Midwest_Region.geojson");
+		Scanner scanner;
+		try {
+			scanner = new Scanner(file);
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
